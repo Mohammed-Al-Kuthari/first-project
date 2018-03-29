db5b1a5dd3b788d07c30ed5be4152510f161099f
Mohammed_Kuthari <iiiwea120@gmail.com>
2018-03-29 23:24:02 +0300

    test commit from vs_code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-windpw.onload = function () {
-    "use strict";
-}
+new_h2 = document.createElement("h2");
+new_h2.append("Change By Javascript");
+document.querySelector("header").replaceChild(new_h2,document.querySelector("header").childNodes[1]);
