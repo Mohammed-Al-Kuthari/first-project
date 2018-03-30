c2bb203590c3ea273f98476e4b36e183d28dad38
Mohammed_Kuthari <iiiwea120@gmail.com>
2018-03-30 15:17:50 +0300

    add php file

---
+++ prosess/check.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+
+    class humen() {
+        public $name;
+        public $age;
+        public $weight;
+        public $hight;
+
+        public function calc($this->$weight,$this->$hight) {
+            echo $this->$hight / $this->$weight
+        }
+    }
+?>
